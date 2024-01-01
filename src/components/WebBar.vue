f1701e56b5dc3fc23c36ca587996f068a1ae1b42
hai <xgkg0344@naver.com>
2024-01-01 19:56:54 +0900

    feat: 생성 WebBar.vue

---
+++ src/components/WebBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <v-app-bar color="grey-lighten-4" flat height="72">
+        <!--<v-avatar> 홈버튼 이미지 넣을경우
+        <img src="홈버튼 이미지.jpg" alt="takoyaki Image">
+      </v-avatar>-->
+      
+        <router-link to="/">
+          <v-btn class="me-2" >home</v-btn>
+        </router-link>  
+        <!--<me-2> 오른쪽 여백을 생성하여 두 번째 버튼과의 간격이 생긴다-->
+        <router-link to="/writeform">
+          <v-btn class="me-2">글 쓰기</v-btn>
+        </router-link>
+      
+        <v-spacer></v-spacer>
+      
+        <v-col class="text-right">
+          <router-link to="/mypage">
+            <v-btn class="me-2">
+              <v-icon>mdi-account</v-icon>
+            </v-btn>
+          </router-link>
+          <v-btn>login</v-btn>
+        </v-col>
+      
+      </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
