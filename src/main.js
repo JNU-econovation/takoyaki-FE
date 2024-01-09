4756a7915904b88a5c094e124c7b5bae35fb63cc
hai <xgkg0344@naver.com>
2024-01-09 20:06:05 +0900

    feat: HTTP 요청에 인증 정보 포함하도록 설정 추가

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router';
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
+axios.defaults.withCredentials = true;
 Vue.prototype.$axios=axios
 //main.js에서 한번만 import해주면 프로젝트 전역에서 사용할 수 있다.이 두 문장은 각 컴포넌트에서마다 import해서 가져올 필요없이,this.$axios를 통해 axios 사용가능
 
