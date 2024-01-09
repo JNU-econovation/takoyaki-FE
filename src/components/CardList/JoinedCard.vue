ef2cbbe09bba765e1adad953768f6d1ec0765131
gyuminzzz <gminerva@kakao.com>
2024-01-10 02:50:46 +0900

    fix: 변수명 수정

--- src/components/CardList/JoinedCard.vue
+++ src/components/CardList/JoinedCard.vue
@@ -2,9 +2,11 @@
     <div class="card-list">
       <div v-for="(card, index) in cards" :key="index" class="card">
         <h3>{{ card.title }}</h3>
+
         <p>카테고리: {{ card.category }}</p>
-        <p>활동 지역: {{ card.region }}</p>
-        <a :href="card.contactUrl" target="_blank"><v-btn color="primary">연락하기</v-btn></a>
+        <p>활동 지역: {{ card.location }}</p>
+        
+        <a :href="card.contact" target="_blank"><v-btn>연락하기</v-btn></a>
       </div>
     </div>
   </template>
@@ -15,7 +17,7 @@
       return {
         cards: [
           // 카드 데이터
-          { title: '제목 1', category: '카테고리1', region: '지역1', contactUrl: 'https://example.com' },
+          { title: '제목 1', category: '카테고리1', location: '지역', contact: 'https://example.com' },
           // 추가 카드 데이터...
         ]
       };
