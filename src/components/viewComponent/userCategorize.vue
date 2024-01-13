6c3d2993e941a8a9e28d249d4178fef9a6fb630c
hai <xgkg0344@naver.com>
2024-01-13 22:41:36 +0900

    feat: user_type 분류 기능 추가

---
+++ src/components/viewComponent/userCategorize.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <TakoView v-if="user_type === 'TAKO'" :party_id="party_id"></TakoView>
+    <YakiView v-else-if="user_type === 'YAKI'" :party_id="party_id"></YakiView>
+    <OtherView v-else-if="user_type =='OTHER'" :party_id="party_id"></OtherView>
+    <loginFalse v-else :party_id="party_id"></loginFalse>
+  </div>
+</template>
+
+<script>
+import TakoView from './TakoView.vue'
+import YakiView from './YakiView.vue'
+import OtherView from './OtherView.vue'
+import loginFalse from './loginFalse.vue'
+
+export default {
+  props:['party_id'],
+  
+  components: {
+    'TakoView': TakoView,
+    'YakiView': YakiView,
+    'OtherView': OtherView,
+    'loginFalse': loginFalse,
+  },
+
+  data() {
+    return{
+    user_type:'',
+    }
+  },
+
+  created() {
+    this.$axios.get(process.env.VUE_APP_TAKOYAKI_API + 'parties/' + this.party_id + "?login=true")
+      .then((response) => {
+        console.log(response)
+        this.user_type = response.data.data.user_type;
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+}
+
+</script>
+
+<style>
+
+</style>
