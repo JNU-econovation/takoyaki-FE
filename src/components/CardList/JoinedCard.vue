7617bf012c74f574df88bf545d6170e536c44f1d
hai <xgkg0344@naver.com>
2024-01-15 00:39:22 +0900

    feat: 참여한 팟 리스트 구현

--- src/components/CardList/JoinedCard.vue
+++ src/components/CardList/JoinedCard.vue
@@ -1,31 +1,57 @@
 <template>
   <div class="card-list">
-    <div
-      v-for="(card, index) in cards"
-      :key="index"
-      class="card"
-    >
-      <h3>{{ card.title }}</h3>
-
-      <p>카테고리: {{ card.category }}</p>
-      <p>활동 지역: {{ card.location }}</p>
+    <div class="card">
+      <h3>{{ title }}</h3>
+     
+      <v-row>
+        <v-col class="text-right">{{ category }}</v-col>
+        <v-col cols='auto' class="text-right">{{ area }}</v-col>
+      </v-row>
+  
+      
         
-        <a :href="card.contact" target="_blank"><v-btn style="width: 250px; position: relative;">연락하기</v-btn></a>
-      </div>
+      <v-btn style="width: 250px; position: relative;">연락하기</v-btn>
     </div>
-  </template>
+  </div>
+</template>
   
   <script>
   export default {
+    props: ['party_id'],
+
     data() {
       return {
-        cards: [
-          // 카드 데이터
-          { title: '제목 1', category: '카테고리1', location: '지역', contact: 'https://example.com' },
-          // 추가 카드 데이터...
-        ]
+        title: '',
+        closingDate: '',
+        category: '',
+        area: '',
+        competitionRate: '',
+        closed_date: '',
+        //this.props,
+        // props: ['propsdata'] //item.party_id받음
       };
-    }
+    },
+    created() {
+      //단일 팟 조회
+      this.$axios.get(process.env.VUE_APP_TAKOYAKI_API + "parties/" + this.party_id + "?login=true")
+        .then((response) => {
+          /* eslint-disable *//* eslint-disable */console.log(...oo_oo(`50331448_40_28_40_84_4`, ...oo_oo(`268278020_39_8_39_29_4`, response)));
+          this.title = response.data.data.title;
+          this.closingDate = response.data.data.planned_closing_date;
+          this.category = response.data.data.category;
+          this.area = response.data.data.activity_location;
+          this.competitionRate = response.data.data.competition_rate;
+          this.closed_date = response.data.data.closed_date;
+
+        })
+        .catch((error) => {
+          /* eslint-disable *//* eslint-disable */console.log(...oo_oo(`50331448_50_28_50_81_4`, ...oo_oo(`268278020_48_8_48_26_4`, error)));
+        })
+    },
+    methods: {
+
+    },
+
   };
   </script>
   
--- src/views/ParticipatedPod.vue
+++ src/views/ParticipatedPod.vue
@@ -29,20 +29,47 @@
               </v-card>
             </v-sheet>
           </v-col>
-        <v-col>
-          <v-sheet min-height="70vh" rounded="lg" width="700px" class="MyPageBG">
-            <!--화면 상단 부분-->
-            <v-row>
-              <v-col>
-                <h1>내가 신청한 팟</h1>
-                <br>
-                <v-row>
-                    <v-btn class="me-3">대기중</v-btn>
-                    <v-btn>수락</v-btn>
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+              width="700px"
+              class="MyPageBG"
+            >
+              <!--화면 상단 부분-->
+              <v-row>
+                <v-col>
+                  <h1>내가 신청한 팟</h1>
+                  <br>
+                  <v-row>
+                    <v-btn
+                      class="me-3"
+                      @click="waiting"
+                    >
+                      대기중
+                    </v-btn>
+                    <v-btn @click="accepted">
+                      수락
+                    </v-btn>
+                    <!--대기중누르면 누른표시나게 색깔을 다르게 한다거나 추가해야함-->
                   </v-row>
+                </v-col> 
+              </v-row>
+              <v-row>
+                <v-col 
+                  v-for="item in list"
+                  :key="item.party_id"
+                  cols="12"
+                  class="me-7"
+                  offset-sm=""
+                >
+                  <!--키로 각각 모든 카드 리스트의 id를 가져옴 -->
+                  <router-link :to="{ name: 'userCategorize', params: { party_id: item.party_id } }">
+                    <BasicCard :party_id="item.party_id" />
+                  <!--받은 키로 BasicCard에 props-->
+                  </router-link>
                 </v-col>
               </v-row>
-
               <!--화면의 중앙에 구분선-->
               <v-row align="center">
                 <v-col>
@@ -55,7 +82,22 @@
                   <h1>마감+수락된 팟(연락처가 공개되었어요!)</h1>
                 </v-col>
               </v-row>
-              <JoinedCard />
+
+              <v-row>
+                <v-col 
+                  v-for="closedItem in closedList"
+                  :key="closedItem.party_id"
+                  cols="12"
+                  class="me-7"
+                  offset-sm=""
+                >
+                  <!--키로 각각 모든 카드 리스트의 id를 가져옴 -->
+                  <router-link :to="{ name: 'userCategorize', params: { party_id: closedItem.party_id } }">
+                    <JoinedCard :party_id="closedItem.party_id" />
+                    <!--받은 키로 BasicCard에 props-->
+                  </router-link>
+                </v-col>
+              </v-row>
             </v-sheet>
           </v-col>
         </v-row>
@@ -67,14 +109,58 @@
 
 <script>
 import JoinedCard from '@/components/CardList/JoinedCard.vue';
+import BasicCard from '@/components/CardList/BasicCard.vue';
 export default {
   components: {
-    'JoinedCard':JoinedCard
+    'JoinedCard':JoinedCard,
+    'BasicCard':BasicCard,
   },
   data() {
     return {
       mypage: ['나의 정보', '내가 연팟', '내가 참여한 팟'],
-      routerMypage: ['my-information', 'written-party', 'participated-party']
+      routerMypage: ['my-information', 'written-party', 'participated-party'],
+      list:[],
+      closedList:[],
+    }
+  },
+  created() {
+    this.$axios.get(process.env.VUE_APP_TAKOYAKI_API + 'parties?type=not_closed_waiting&number=3&page=1')
+      .then((response) => {
+        /* eslint-disable */console.log(...oo_oo(`2121293739_117_8_117_29_4`,response));
+        this.list = response.data.data;
+      })
+      .catch((error) => {
+        /* eslint-disable */console.log(...oo_oo(`2121293739_121_8_121_26_4`,error));
+      }),
+      this.$axios.get(process.env.VUE_APP_TAKOYAKI_API + 'parties?type=closed&number=3&page=1')
+        .then((response) => {
+          /* eslint-disable */console.log(...oo_oo(`2121293739_125_10_125_31_4`,response));
+          this.closedList = response.data.data;
+        })
+        .catch((error) => {
+          /* eslint-disable */console.log(...oo_oo(`2121293739_129_10_129_28_4`,error));
+        })
+      },
+  methods: {
+    waiting() {
+      this.$axios.get(process.env.VUE_APP_TAKOYAKI_API + 'parties?type=not_closed_waiting&number=3&page=1')
+        .then((response) => {
+          /* eslint-disable */console.log(...oo_oo(`2121293739_136_10_136_31_4`,response));
+          this.list=response.data.data;
+        })
+        .catch((error) => {
+          /* eslint-disable */console.log(...oo_oo(`2121293739_140_10_140_28_4`,error));
+        })
+    },
+    accepted() {
+      this.$axios.get(process.env.VUE_APP_TAKOYAKI_API + 'parties?type=not_closed_accepted&number=3&page=1')
+        .then((response) => {
+          /* eslint-disable */console.log(...oo_oo(`2121293739_146_10_146_31_4`,response));
+          this.list = response.data.data;
+        })
+        .catch((error) => {
+          /* eslint-disable */console.log(...oo_oo(`2121293739_150_10_150_28_4`,error));
+        })
     }
   }
 }
