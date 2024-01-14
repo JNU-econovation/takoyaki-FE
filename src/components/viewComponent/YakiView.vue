15978495002c8369cf5c423948a475270ff81c2e
hai <xgkg0344@naver.com>
2024-01-15 01:29:01 +0900

    feat: yaki_status에 따른 yakiView 추가

--- src/components/viewComponent/YakiView.vue
+++ src/components/viewComponent/YakiView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <v-container>
+
       <div v-if="closed_date==null">
         <h1> {{ title }} </h1>
         <br><br>
@@ -9,19 +10,37 @@
             <h3> 팟 정보 </h3>
           </v-col>
           <v-col>{{ nickname }}</v-col>
-          <v-col>
-            <v-row>
-              {{ closingDate }} 마감
-            </v-row>
-            <v-row>
-              <div class="complete">
-                신청 완료
-              </div> 
-              <v-btn @click="cancelApplication">
-                신청 취소
-              </v-btn>
-            </v-row>
-          </v-col>
+
+          <div v-if="yaki_status === 'WAITING'">
+            <v-col>
+              <v-row>
+                {{ closingDate }} 마감
+              </v-row>
+              <v-row>
+                <div class="complete">
+                  신청 완료
+                </div> 
+                <v-btn @click="cancelApplication">
+                  신청 취소
+                </v-btn>
+              </v-row>
+            </v-col>
+          </div>
+          <div v-else>
+            <v-col>
+              <v-row>
+                {{ closingDate }} 마감
+              </v-row>
+              <v-row>
+                <div class="complete">
+                  수락되었어요!
+                </div> 
+                <v-btn @click="leaving">
+                  팟 나가기
+                </v-btn>
+              </v-row>
+            </v-col>
+          </div>
         </v-row>
       </div>
       <div v-else>
@@ -38,6 +57,7 @@
       </div>
       <informationParty :party_id="party_id" />
     </v-container>
+
   </div>
 </template>
 
@@ -57,6 +77,7 @@ export default {
       nickname: '',
       closingDate: '',
       closed_date:'',
+      yaki_status:'',
     }
   },
 
@@ -69,6 +90,7 @@ export default {
         this.nickname = response.data.data.nickname;
         this.closingDate = response.data.data.planned_closing_date;
         this.closed_date = response.data.data.closed_date;
+        this.yaki_status=response.data.data.yaki_status;
       })
       .catch((error) => {
         /* eslint-disable *//* eslint-disable */console.log(...oo_oo(`4008950287_74_28_74_82_4`,...oo_oo(`4036828524_56_8_56_26_4`,error)));
@@ -86,6 +108,16 @@ export default {
           /* eslint-disable *//* eslint-disable */console.log(...oo_oo(`4008950287_86_30_86_85_4`,...oo_oo(`4036828524_68_10_68_28_4`,error)));
         })
       },
+      leaving() {
+        this.$axios.delete(process.env.VUE_APP_TAKOYAKI_API + 'parties/' + this.party_id + "/leaving")
+        .then((response) => {
+          /* eslint-disable *//* eslint-disable */console.log(...oo_oo(`4008950287_82_30_82_88_4`, ...oo_oo(`4036828524_64_10_64_31_4`, response)))
+          this.$router.push({ path: '/' }) //신청취소버튼 클릭시 메인 홈으로 이동
+        })
+        .catch((error) => {
+          /* eslint-disable *//* eslint-disable */console.log(...oo_oo(`4008950287_86_30_86_85_4`, ...oo_oo(`4036828524_68_10_68_28_4`, error)));
+        })
+      }
     }
   }
 
